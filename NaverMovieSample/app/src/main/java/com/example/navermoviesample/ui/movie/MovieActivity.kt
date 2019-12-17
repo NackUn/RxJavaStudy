8bbc014aeb82808ce65f859e9292f9838097c271
NackUn <king4521@naver.com>
2019-12-17 15:55:17 +0900

    movie 생성

---
+++ NaverMovieSample/app/src/main/java/com/example/navermoviesample/ui/movie/MovieActivity.kt
@@ -0,0 +1,6 @@
+package com.example.navermoviesample.ui.movie
+
+import com.example.navermoviesample.R
+import com.example.navermoviesample.base.BaseActivity
+
+class MovieActivity : BaseActivity(R.layout.activity_movie)
---
+++ NaverMovieSample/app/src/main/java/com/example/navermoviesample/ui/movie/MovieFragment.kt
@@ -0,0 +1,7 @@
+package com.example.navermoviesample.ui.movie
+
+import com.example.navermoviesample.base.BaseFragment
+
+class MovieFragment : BaseFragment() {
+
+}
