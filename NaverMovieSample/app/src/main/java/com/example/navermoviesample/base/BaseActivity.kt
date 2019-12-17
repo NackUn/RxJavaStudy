19e5226f8e7ca1ac9ad9ecbd6887f58d35fd979a
NackUn <king4521@naver.com>
2019-12-17 15:51:38 +0900

    base 생성

---
+++ NaverMovieSample/app/src/main/java/com/example/navermoviesample/base/BaseActivity.kt
@@ -0,0 +1,15 @@
+package com.example.navermoviesample.base
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity(
+    @LayoutRes private val layoutRes: Int
+) : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layoutRes)
+    }
+}
