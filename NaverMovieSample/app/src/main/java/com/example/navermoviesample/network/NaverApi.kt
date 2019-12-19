ea204dd193e52d5209af8355ed15c31f7ae35aa2
NackUn <king4521@naver.com>
2019-12-19 17:55:48 +0900

    헤더 추가

--- NaverMovieSample/app/src/main/java/com/example/navermoviesample/network/NaverApi.kt
+++ NaverMovieSample/app/src/main/java/com/example/navermoviesample/network/NaverApi.kt
@@ -3,9 +3,17 @@ package com.example.navermoviesample.network
 import com.example.navermoviesample.vo.SearchResult
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface NaverApi {
+    @Headers(
+        "X-Naver-Client-Id:mPSHgEZRlh0FiYZQW0N3",
+        "X-Naver-Client-Secret:rOdScOfgWl"
+    )
+
     @GET("v1/search/movie.json")
-    fun requestMovies(@Query("query") searchWord: String): Call<SearchResult>
+    fun requestMovies(
+        @Query("query") searchWord: String
+    ): Call<SearchResult>
 }
