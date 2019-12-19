b78e3a1868dc34e516f968f51a77bd69434fa32a
NackUn <king4521@naver.com>
2019-12-19 17:57:52 +0900

    MovieRecyclerViewExt 추가

---
+++ NaverMovieSample/app/src/main/java/com/example/navermoviesample/ui/movie/MovieRecyclerViewExt.kt
@@ -0,0 +1,14 @@
+package com.example.navermoviesample.ui.movie
+
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.navermoviesample.base.BaseRecyclerView
+import com.example.navermoviesample.vo.MovieItem
+
+@BindingAdapter("setItems")
+fun RecyclerView.setItems(items: List<MovieItem>) {
+    (this.adapter as? BaseRecyclerView.BaseAdapter<*, *>)?.run {
+        setItems(items)
+        notifyDataSetChanged()
+    }
+}
