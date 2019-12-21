bf365f831aa69a16116ad9157c05a1407fdf28a3
NackUn <king4521@naver.com>
2019-12-22 02:40:53 +0900

    retrofitService run 제거 및 SuppressLint(CheckResult) 추가

--- NaverMovieSample/app/src/main/java/com/example/navermoviesample/data/remote/RemoteDataSource.kt
+++ NaverMovieSample/app/src/main/java/com/example/navermoviesample/data/remote/RemoteDataSource.kt
@@ -1,5 +1,6 @@
 package com.example.navermoviesample.data.remote
 
+import android.annotation.SuppressLint
 import com.example.navermoviesample.data.DataSource
 import com.example.navermoviesample.network.NaverApi
 import com.example.navermoviesample.vo.MovieItem
@@ -9,20 +10,19 @@ import io.reactivex.schedulers.Schedulers
 class RemoteDataSource(
     private val retrofitService: NaverApi
 ) : DataSource {
+    @SuppressLint("CheckResult")
     override fun requestMovies(
         searchWord: String,
         onSearchSuccess: (List<MovieItem>) -> Unit,
         onError: (Throwable) -> Unit
     ) {
-        retrofitService.run {
-            this.requestMovies(searchWord)
-                .subscribeOn(Schedulers.computation())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                    { searchResult -> onSearchSuccess(searchResult.movieItems) },
-                    { thowable -> onError(thowable) }
-                )
-        }
+        retrofitService.requestMovies(searchWord)
+            .subscribeOn(Schedulers.computation())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { searchResult -> onSearchSuccess(searchResult.movieItems) },
+                { thowable -> onError(thowable) }
+            )
     }
 
 }
